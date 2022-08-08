391c621340e17a712368e32d3eb8ed01c7bbd195
truthprice <truthop@gmail.com>
2022-08-07 21:13:06 -0400

    cleaned logic file

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -55,31 +55,10 @@ function createFeatures(earthquakeData) {
     pointToLayer: createCircles
   });
   
-  // Send our earthquakes layer to the createMap function/
-//   createMap(earthquakes, earthquakeData);
-// }
-
-// function createMap(earthquakes, earthquakeData) {
-
     // Create the base layers.
   var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
-  
-  // var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-  //     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  // });
-  
-    // Create a baseMaps object.
-    // var baseMaps = {
-    //   "Street Map": street,
-    //   "Topographic Map": topo
-    // };
-  
-    // // Create an overlay object to hold our overlay.
-    // var overlayMaps = {
-    //   Earthquakes: earthquakes
-    // };
 
       // Create our map, giving it the streetmap and earthquakes layers to display on load.
 var myMap = L.map("map", {
